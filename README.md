9c555dff228d0ea75f3593be7ac2a562a1687bd5
deepakgangwr <128113763+deepakgangwr@users.noreply.github.com>
2024-06-18 19:46:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weatherwhiz
+ 
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e7139a9623bbb3992dd8511357dc7450",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
